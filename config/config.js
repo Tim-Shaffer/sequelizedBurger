3cddcfe23f07aa1f01884f700a427ca0787d9711
Tim-Shaffer <tcshaf24@ptd.net>
2020-04-02 23:46:00 -0400

    updated environment variables

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
   "development": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
-    "database": "burgers2_db",
+    "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
